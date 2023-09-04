04e83704dce5810de4dd7ca4a4c68d8807370ebc
petrenko@binaryanvil.com <mpetrenko@binaryanvil.com>
2023-09-04 11:15:17 +0300

    first commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+
+const Header = props => {
+	return (
+		<header className="header">
+        <img src={props.img} alt="logo" />
+        <h1>Investment Calculator</h1>
+      </header>
+	)
+} 
+
+export default Header
